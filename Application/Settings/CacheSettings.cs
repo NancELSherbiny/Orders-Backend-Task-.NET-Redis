6e0ed4e6d70838c9d74d797b8049d0404ce37eef
Nancy EL Sherbiny <103130605+NancELSherbiny@users.noreply.github.com>
2025-11-29 01:25:29 +0200

    Cashe Settings

---
+++ Application/Settings/CacheSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Settings
+{
+    public class CacheSettings
+    {
+        public int OrderTTLSeconds { get; set; }
+    }
+}
+
