7b94650d52060508cc69aa2c840c2ae196af2821
Nancy EL Sherbiny <103130605+NancELSherbiny@users.noreply.github.com>
2025-11-29 01:24:31 +0200

    API.mapping

---
+++ API/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Application.DTOs;
+using DAL.Models;
+
+namespace API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Create
+            CreateMap<OrderCreateDto, Order>();
+
+            // Update
+            CreateMap<OrderUpdateDto, Order>();
+
+            // Response
+            CreateMap<Order, OrderResponseDto>();
+        }
+    }
+}
